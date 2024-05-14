a19395a175b7cf84ec8cd419faa206c8e6e4b857
Nannita1403 <nannimagi@gmail.com>
2024-05-14 15:53:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { connectDB } = require("./src/config/db");
 connectDB();
 
 const app = express();
+app.use(express.json());
 app.use("*", (req, res, next) => {
     return res.status(404).json("Route not found")
 })
