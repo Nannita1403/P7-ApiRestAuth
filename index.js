56ba80d20bf58e28b4a01debef870cbe362f32c0
Nannita1403 <nannimagi@gmail.com>
2024-05-14 15:33:10 +0200

    update conect bbdd ok

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const { connectDB } = require("./src/config/db");
+connectDB();
+
+const app = express();
+app.use("*", (req, res, next) => {
+    return res.status(404).json("Route not found")
+})
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+})
