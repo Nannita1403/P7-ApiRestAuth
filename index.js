b120845baafbdb138468426db2bb2246085efefc
Nannita1403 <nannimagi@gmail.com>
2024-05-16 00:26:53 +0200

    ok

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const express = require("express");
 const { connectDB } = require("./src/config/db");
 const gamesRouter = require("./src/api/routes/games");
 const platformRouter = require("./src/api/routes/platforms");
-const cors = require("cors");
 const usersRoutes = require("./src/api/routes/users");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 connectDB();
 app.use("/api/v1/users", usersRoutes);
